1f69bc734350031be24f76dee59bcb83093958fb
Gayeong98 <gayeong122@gmail.com>
2023-04-24 15:02:58 +0900

    webSecurity;

---
+++ src/main/java/com/kiosk/kioskback/config/WebSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.kiosk.kioskback.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.kiosk.kioskback.filter.JwtAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired private JwtAuthenticationFilter jwtAuthenticationFilter;
+    
+    @Bean
+    protected SecurityFilterChain config(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .cors().and()
+            .csrf().disable()
+            .httpBasic().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .authorizeRequests().requestMatchers("/**", "/**").permitAll()
+            .anyRequest().authenticated();
+
+            httpSecurity.addFilter(jwtAuthenticationFilter);
+
+        return httpSecurity.build();
+    }
+
+    // @Bean
+    // protected WebSecurityCustomizer webSecurityCustomizer() {
+    //     return (web) -> web.ignoring()
+    //         .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/swagger/**", "/v2/api-docs", "/webjars/**");
+    // }
+
+}
