a11aa5d28149ab417f4b17d5e472a7a8dd8a53a8
YuHoSeong <ghtjd026@gmail.com>
2023-04-24 17:12:09 +0900

    modifed: CategoryEntity className

--- src/main/java/com/kiosk/kioskback/entity/CategoryEntity.java
+++ src/main/java/com/kiosk/kioskback/entity/CategoryEntity.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class MenuCategoryEntity {
+public class CategoryEntity {
 
     private int menuCategoryId;
     private String menuCategoryName;
