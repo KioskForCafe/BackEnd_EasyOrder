e9b6e5bb9ad9fd03f29bd0fd0804206856b7a8fd
Gayeong98 <gayeong122@gmail.com>
2023-04-24 15:14:46 +0900

    webSecurity

--- src/main/java/com/kiosk/kioskback/config/WebSecurityConfig.java
+++ src/main/java/com/kiosk/kioskback/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -31,10 +32,10 @@ public class WebSecurityConfig {
         return httpSecurity.build();
     }
 
-    // @Bean
-    // protected WebSecurityCustomizer webSecurityCustomizer() {
-    //     return (web) -> web.ignoring()
-    //         .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/swagger/**", "/v2/api-docs", "/webjars/**");
-    // }
+    @Bean
+    protected WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+            .requestMatchers("/swagger-ui.html", "/swagger-resources/**", "/swagger/**", "/v2/api-docs", "/webjars/**");
+    }
 
 }
