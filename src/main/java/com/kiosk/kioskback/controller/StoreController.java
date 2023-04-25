89ca7ea314e6a1aafe0edce38b1db0d5fcf8942c
YuHoSeong <ghtjd026@gmail.com>
2023-04-25 16:11:41 +0900

    feat: 매장 등록 구현
    StoreService(postStore), StoreController(postStore), PostStoreResponseDto, PostStoreDto 추가

--- src/main/java/com/kiosk/kioskback/controller/StoreController.java
+++ src/main/java/com/kiosk/kioskback/controller/StoreController.java
@@ -5,12 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kiosk.kioskback.common.constants.ApiPattern;
+import com.kiosk.kioskback.dto.request.store.PostStoreDto;
 import com.kiosk.kioskback.dto.response.ResponseDto;
 import com.kiosk.kioskback.dto.response.store.GetStoreResponseDto;
+import com.kiosk.kioskback.dto.response.store.PostStoreResponseDto;
 import com.kiosk.kioskback.service.StoreService;
 
 import io.swagger.annotations.Api;
@@ -23,8 +27,8 @@ public class StoreController {
     @Autowired private StoreService storeService;
 
     private final String GET = "";
+    private final String POST = "";
 
-    //^ 매장 전체 조회
     @ApiOperation(
         value = "전체 매장 리스트 가져오기",
         notes = "Reqeust Header Authorization에 Bearer JWT를 포함하여 요청을 하면, 성공시 전체 매장 리스트를 최신순으로 반환, 실패시 실패 메세지를 반환")
@@ -33,6 +37,17 @@ public class StoreController {
         ResponseDto<List<GetStoreResponseDto>> response = storeService.getStore(userId);
         return response;
     }
+
+    @ApiOperation(
+        value = "전체 매장 리스트 가져오기",
+        notes = "Reqeust Header Authorization에 Bearer JWT를 포함하고 Request Body에 매장이름, 오픈시간, 마감시간, 로고 URL, 이미지 URL을 포함하여 요청을 하면, 성공시 매장 정보를 반환, 실패시 실패 메세지를 반환")
+    @PostMapping(POST)
+    public ResponseDto<PostStoreResponseDto> postStore(@AuthenticationPrincipal String userId, @RequestBody PostStoreDto requestbody){
+        ResponseDto<PostStoreResponseDto> response = storeService.postStore(userId, requestbody);
+        return response;
+    }
+
+
 
     
 }
--- src/main/java/com/kiosk/kioskback/service/StoreService.java
+++ src/main/java/com/kiosk/kioskback/service/StoreService.java
@@ -2,12 +2,16 @@ package com.kiosk.kioskback.service;
 
 import java.util.List;
 
+import com.kiosk.kioskback.dto.request.store.PostStoreDto;
 import com.kiosk.kioskback.dto.response.ResponseDto;
 import com.kiosk.kioskback.dto.response.store.GetStoreResponseDto;
+import com.kiosk.kioskback.dto.response.store.PostStoreResponseDto;
 
 public interface StoreService {
     
     //^ 매장 전체 조회
     public ResponseDto<List<GetStoreResponseDto>> getStore(String userId);
 
+    //^ 매장 등록
+    public ResponseDto<PostStoreResponseDto> postStore(String userId, PostStoreDto requestbody);
 }
