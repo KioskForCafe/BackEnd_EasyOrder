95e739391c1ed083bd75aca926ace5dfbdc688cd
YuHoSeong <ghtjd026@gmail.com>
2023-04-28 17:16:18 +0900

    fix: OrderDetailLogRepository(findByBusinessByTime)
    GetAnalysisBusinessResponseDto의 형식에 맞도록 수정

--- src/main/java/com/kiosk/kioskback/entity/resultSet/BusinessByTimeResultSet.java
+++
@@ -1,7 +0,0 @@
-package com.kiosk.kioskback.entity.resultSet;
-
-public interface BusinessByTimeResultSet {
-    public int getOrderId();
-    public int getSaleAmount();
-    public String getTime();
-}
