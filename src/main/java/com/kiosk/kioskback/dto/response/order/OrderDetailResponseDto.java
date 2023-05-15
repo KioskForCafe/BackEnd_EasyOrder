753c0b4f71984a24eed2b1a74133a5ffa719cf20
Noa <114126439+susu144178@users.noreply.github.com>
2023-05-15 17:38:20 +0900

    Kimseonwu/#47 (#74)
    * ing
    
    * test
    
    * feat: 카테고리 리스트 조회/카테고리 등록/카테고리 수정/카테고리 삭제
    
    카테고리 수정/삭제는 오류가 일어날 수도 있음 후에 더 체크할 예정
    
    resolve: #32,#33,#34,#35
    
    * test
    
    * merge
    
    * merge
    
    * merge
    
    * test

---
+++ src/main/java/com/kiosk/kioskback/dto/response/order/OrderDetailResponseDto.java
@@ -0,0 +1,35 @@
+package com.kiosk.kioskback.dto.response.order;
+
+import java.util.List;
+
+import com.kiosk.kioskback.entity.OptionEntity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@ApiModel(value = "장바구니 메뉴 Response Body - data")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDetailResponseDto {
+
+    @ApiModelProperty(value = "상세 주문 번호", example = "1", required = true)
+    private int orderDetailId;
+
+    @ApiModelProperty(value = "메뉴 이름", example = "아메리카노", required = true)
+    private String menuName;
+
+    @ApiModelProperty(value = "메뉴 가격", example = "2000", required = true)
+    private int menuPrice;
+
+    @ApiModelProperty(value = "옵션 리스트", example = "list", required = false)
+    private List<OptionEntity> optionList;
+
+    @ApiModelProperty(value = "상세 주문 수량", example = "3", required = true)
+    private int orderDetailCount;
+
+    
+}
