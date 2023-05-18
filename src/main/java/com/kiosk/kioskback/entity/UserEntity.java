2d5b2cb4feba07b58bd4f29e4c611b161176f437
YuHoSeong <ghtjd026@gmail.com>
2023-05-18 12:07:57 +0900

    feat: UserEntity(dto) 변경 this.isAdmin = true  -> this.isAdmin = dto.isAdmin()

--- src/main/java/com/kiosk/kioskback/entity/UserEntity.java
+++ src/main/java/com/kiosk/kioskback/entity/UserEntity.java
@@ -34,7 +34,7 @@ public class UserEntity {
         this.password = dto.getPassword();
         this.userEmail = dto.getUserEmail();
         this.telNumber = dto.getTelNumber();
-        this.isAdmin = true;
+        this.isAdmin = dto.isAdmin();
         this.createdAt = new Date();
 
     }
