b1fd94e84d5879af16a27c5af5701c6f2b1a7165
Gayeong98 <114125163+Gayeong98@users.noreply.github.com>
2023-05-25 17:44:55 +0900

    Kimgayeong (#94)
    * feat: postOrderLog
    
    * fix: OrderDetailLogRepository- 날짜 관련 수정, user_id 관련 쿼리
     수정
    
    ---------
    
    Co-authored-by: first-project <121538196+comet7406@users.noreply.github.com>
    Co-authored-by: YuHoSeong <ghtjd026@gmail.com>

--- src/main/java/com/kiosk/kioskback/repository/OrderDetailLogRepository.java
+++ src/main/java/com/kiosk/kioskback/repository/OrderDetailLogRepository.java
@@ -17,7 +17,7 @@ import com.kiosk.kioskback.entity.resultSet.UserTop10ResultSet;
 public interface OrderDetailLogRepository extends JpaRepository<OrderDetailLogEntity,Integer>{
   @Query(value = "SELECT category_id categoryId, category_name categoryName, count(category_id) saleCount, sum(price_with_option) totalPrice "
   + "FROM order_detail_log "
-  + "WHERE store_id = :storeId AND created_at BETWEEN :startedAt AND :endedAt "
+  + "WHERE store_id = :storeId AND created_at BETWEEN :startedAt AND :endedAt + INTERVAL 1 DAY "
   + "GROUP BY category_id, category_name "
   + "ORDER BY saleCount DESC , totalPrice DESC ", nativeQuery = true
   )
@@ -26,23 +26,21 @@ public interface OrderDetailLogRepository extends JpaRepository<OrderDetailLogEn
   public List<OrderDetailLogEntity> findByStoreId(int storeId);
   @Query(value = "SELECT menu_id menuId, menu_name menuName, count(menu_name) saleCount, sum(price_with_option) totalPrice "
   + "FROM order_detail_log "
-  + "WHERE store_id = :storeId AND created_at BETWEEN :startedAt AND :endedAt "
+  + "WHERE store_id = :storeId AND created_at BETWEEN :startedAt AND :endedAt + INTERVAL 1 DAY "
   + "GROUP BY menu_id, menu_name "
   + "ORDER BY saleCount DESC , totalPrice DESC ", nativeQuery = true
   )
   public List<ByMenuResultSet> findAllAnalysisByMenu(@Param("storeId") int storeId,@Param("startedAt") Date startedAt,@Param("endedAt") Date endedAt);
 
-  @Query(value = "SELECT count(*) "
+  @Query(value = "SELECT count(distinct user_id) "
   + "FROM order_log "
-  + "WHERE store_id = :storeId "
-  + "GROUP BY user_id " , nativeQuery = true
+  + "WHERE store_id = :storeId ", nativeQuery = true
   )
   public Integer countTotalUserByStoreId(@Param("storeId") int storeId);
   
-  @Query(value = "SELECT count(*) "
+  @Query(value = "SELECT count(distinct user_id) "
   + "FROM order_log "
-  + "WHERE store_id = :storeId AND user_id IN (SELECT user_id FROM user WHERE created_at BETWEEN :startedAt AND :endedAt) AND created_at BETWEEN :startedAt AND :endedAt "
-  + "GROUP BY user_id " , nativeQuery = true
+  + "WHERE store_id = :storeId AND user_id IN (SELECT user_id FROM user WHERE created_at BETWEEN :startedAt AND :endedAt + INTERVAL 1 DAY) AND created_at BETWEEN :startedAt AND :endedAt + INTERVAL 1 DAY ", nativeQuery = true
   )
   public Integer countNewUserByStoreId(@Param("storeId") int storeId,@Param("startedAt") Date startedAt,@Param("endedAt") Date endedAt);
   
@@ -50,7 +48,7 @@ public interface OrderDetailLogRepository extends JpaRepository<OrderDetailLogEn
   + "FROM order_log a "
   + "LEFT JOIN point b "
   + "ON a.tel_number = b.tel_number "
-  + "WHERE a.store_id = :storeId AND a.created_at BETWEEN :startedAt AND :endedAt "
+  + "WHERE a.store_id = :storeId AND a.created_at BETWEEN :startedAt AND :endedAt + INTERVAL 1 DAY "
   + "GROUP BY a.user_id, a.order_log_id, a.user_name, a.tel_number "
   + "ORDER BY amountPayment DESC "
   , nativeQuery = true
@@ -64,7 +62,7 @@ public interface OrderDetailLogRepository extends JpaRepository<OrderDetailLogEn
   + "FROM order_detail_log a "
   + "JOIN order_log b "
   + "ON a.order_log_id = b.order_log_id "
-  + "WHERE a.store_id = :storeId AND a.created_at BETWEEN :startedAt AND :endedAt "
+  + "WHERE a.store_id = :storeId AND a.created_at BETWEEN :startedAt AND :endedAt + INTERVAL 1 DAY "
   + "GROUP BY a.order_log_id "
   + ") c "
   + "GROUP BY time "
@@ -79,7 +77,7 @@ public interface OrderDetailLogRepository extends JpaRepository<OrderDetailLogEn
   + "FROM order_detail_log a "
   + "JOIN order_log b "
   + "ON a.order_log_id = b.order_log_id "
-  + "WHERE a.store_id = :storeId AND a.created_at BETWEEN :startedAt AND :endedAt "
+  + "WHERE a.store_id = :storeId AND a.created_at BETWEEN :startedAt AND :endedAt + INTERVAL 1 DAY "
   + "GROUP BY a.order_log_id "
   + ") c "
   , nativeQuery = true
