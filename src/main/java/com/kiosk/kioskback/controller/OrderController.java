396910297b21c79fd467045762779ff36daddb8a
Gayeong98 <114125163+Gayeong98@users.noreply.github.com>
2023-06-01 16:57:03 +0900

    Kimgayeong (#98)
    * feat: OrderLog-State별 count 전체 조회 기능
    
    ---------
    
    Co-authored-by: first-project <121538196+comet7406@users.noreply.github.com>

--- src/main/java/com/kiosk/kioskback/controller/OrderController.java
+++ src/main/java/com/kiosk/kioskback/controller/OrderController.java
@@ -21,6 +21,7 @@ import com.kiosk.kioskback.dto.response.ResponseDto;
 import com.kiosk.kioskback.dto.response.order.DeleteOrderResponseDto;
 import com.kiosk.kioskback.dto.response.order.GetOrderDetailResponseDto;
 import com.kiosk.kioskback.dto.response.order.GetOrderResponseDto;
+import com.kiosk.kioskback.dto.response.order.GetOrderStateResponseDto;
 import com.kiosk.kioskback.dto.response.order.PatchOrderResponseDto;
 import com.kiosk.kioskback.dto.response.order.PostOrderLogResponseDto;
 import com.kiosk.kioskback.dto.response.order.PostOrderResponseDto;
@@ -40,6 +41,7 @@ public class OrderController {
 
     private final String GET_ORDER_LIST = "/list/{storeId}";
     private final String GET_ORDER_LIST_STATE = "/list/{storeId}/{orderState}";
+    private final String GET_ORDER_STATE_COUNT = "/count/{storeId}";
     private final String GET_ORDER_DETAIL_LIST = "/{orderId}";
     private final String POST_ORDER = "";
     private final String POST_ORDER_LOG = "/log";
@@ -66,6 +68,16 @@ public class OrderController {
         @PathVariable("orderId") int orderId
     ) {
         ResponseDto<List<GetOrderDetailResponseDto>> response = orderService.getOrderDetailList(userId, orderId);
+        return response;
+    }
+
+    @ApiOperation(value = "orderState와 orderState에 따른 주문 갯수 전체를 반환하는 기능", notes = "")
+    @GetMapping(GET_ORDER_STATE_COUNT)
+    public ResponseDto<List<GetOrderStateResponseDto>> getOrderState(
+        @ApiParam(hidden = true)
+        @AuthenticationPrincipal String userId,
+        @PathVariable("storeId") int storeId) {
+            ResponseDto<List<GetOrderStateResponseDto>> response = orderService.getOrderState(userId, storeId);
         return response;
     }
 
